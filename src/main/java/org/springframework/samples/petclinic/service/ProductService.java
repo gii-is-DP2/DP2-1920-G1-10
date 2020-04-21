70ad8a7227032da402ec4707458f44ae1696a057
Félix Jiménez González <feljimgon1@alum.us.es>
2020-04-21 13:49:06 +0200

    [Fix] Arreglada dependencia delete product con booking

--- src/main/java/org/springframework/samples/petclinic/service/ProductService.java
+++ src/main/java/org/springframework/samples/petclinic/service/ProductService.java
@@ -1,9 +1,13 @@
 package org.springframework.samples.petclinic.service;
 
+import java.util.List;
+
 import org.h2.security.auth.AuthConfigException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.samples.petclinic.model.Booking;
 import org.springframework.samples.petclinic.model.Product;
+import org.springframework.samples.petclinic.repository.springdatajpa.BookingRepository;
 import org.springframework.samples.petclinic.repository.springdatajpa.ProductRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +17,9 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository productRepository;
+
+	@Autowired
+	private BookingRepository bookingRepository;
 
 	@Transactional
 	public Iterable<Product> findAll() {
@@ -31,8 +38,12 @@ public class ProductService {
 
 	@Transactional
 	public void delete(Product product) {
+		List<Booking> bookings = bookingRepository.findAllByProductId(product.getId());
 		if (AuthoritiesService.checkAdmin() != true) {
 			throw new AuthConfigException("Debe ser administrador");
+		}
+		for (int i = 0; i < bookings.size(); i++) {
+			bookingRepository.delete(bookings.get(i));
 		}
 		productRepository.delete(product);
 	}
