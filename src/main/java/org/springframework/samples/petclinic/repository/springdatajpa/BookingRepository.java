a695e8f84fc5980c67fe04acd4a690b6e8cb5be5
Félix Jiménez González <feljimgon1@alum.us.es>
2020-05-08 21:29:09 +0200

    [Fix] Añadida query para borrar Product con Booking

--- src/main/java/org/springframework/samples/petclinic/repository/springdatajpa/BookingRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/springdatajpa/BookingRepository.java
@@ -15,4 +15,7 @@ public interface BookingRepository extends CrudRepository<Booking, Integer> {
 
 	@Query("SELECT a FROM Booking a WHERE a.user=:usernameid AND a.producto.id=:productid")
 	Booking findPreviousBooking(@Param("usernameid") String userId, @Param("productid") int productId);
+
+	@Query("SELECT b FROM Booking b WHERE b.producto.id=:productId")
+	List<Booking> findAllByProductId(@Param("productId") Integer productId);
 }
