6e446d5de4d49a18e91f4fa069ca53432263f0ae
Félix Jiménez González <feljimgon1@alum.us.es>
2020-05-10 17:24:46 +0200

    [Fix] Travis E2E

--- src/test/java/org/springframework/samples/petclinic/web/e2e/ProductControllerE2ETests.java
+++ src/test/java/org/springframework/samples/petclinic/web/e2e/ProductControllerE2ETests.java
@@ -14,8 +14,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.samples.petclinic.PetclinicApplication;
 import org.springframework.samples.petclinic.web.ProductController;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -28,6 +30,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
+@ContextConfiguration(classes = PetclinicApplication.class)
 class ProductControllerE2ETests {
 
 	private static final int TEST_PRODUCT_ID = 1;
